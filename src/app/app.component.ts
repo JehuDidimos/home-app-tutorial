aceb17ccc4b46166a4aabb0347e7c1eec2e7a0e6
JehuDidimos <jehudidimos.cs@gmail.com>
2023-07-14 14:26:14 -0500

    Home componenet: component import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  standalone: true,
-  selector: 'app-root',
-  template: `
+    standalone: true,
+    selector: 'app-root',
+    template: `
   <main>
     <header class="brand-name">
       <img class="brand-logo" src="assets\logo.svg" alt="logo" aria-hidden="true">
@@ -13,8 +14,8 @@ import { Component } from "@angular/core";
     </section>
   </main>
   `,
-  styleUrls: ['./app.component.css']
-
+    styleUrls: ['./app.component.css'],
+    imports: [HomeComponent]
 })
 export class AppComponent{
   title = "homes"
